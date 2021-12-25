7d493c67edb0243ba4b1439dacfaa7eec316823c
Christoph <email@ckloth.de>
2021-12-25 18:49:04 +0100

    app/srv: update version info

--- server/modules/rm3config.py
+++ server/modules/rm3config.py
@@ -4,10 +4,10 @@ import time
 # ---------------------------------
 
 APIname    = "jc://remote/"
-APIversion = "v2.0.2"
-APPversion = "v2.7.2"
+APIversion = "v2.0.3"
+APPversion = "v2.7.3"
 APPsupport = [APPversion, 
-             "v2.7.1","v2.7.0"
+             "v2.7.2", "v2.7.1","v2.7.0"
              ]  # other supported versions
 
 # ---------------------------------
