07d707b2851fb775f792d27ac1d8e1cb56f1b4f2
Christoph <email@ckloth.de>
2022-11-03 18:26:18 +0100

    srv: increase API version

--- server/modules/rm3config.py
+++ server/modules/rm3config.py
@@ -4,7 +4,7 @@ import time
 # ---------------------------------
 
 APIname = "jc://remote/"
-APIversion = "v2.0.7"
+APIversion = "v2.1.0"
 APPversion = "v2.7.7"
 APPsupport = [APPversion,
               "v2.7.7"
