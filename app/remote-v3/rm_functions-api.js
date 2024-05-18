c45276fb8c0b23d4a37b869239be3f8277f59813
Christoph <email@ckloth.de>
2024-05-18 14:45:00 +0200

    app: bugfix macro editing

--- app/remote-v3/rm_functions-api.js
+++ app/remote-v3/rm_functions-api.js
@@ -281,7 +281,11 @@ function apiDeviceJsonEdit(device,json_buttons,json_display,display_size) {
 // move position of device or scene in the menu
 function apiDeviceMovePosition_exe(type,device,direction) { appFW.requestAPI( "POST", ["move",type,device,direction], "", apiDeviceMovePosition_get); }
 
-function apiDeviceMovePosition_get(data) { appFW.requestAPI("GET",["list"],"",apiDeviceMovePosition); }
+function apiDeviceMovePosition_get(data) {
+    if (data["REQUEST"]["Return"].indexOf("ERROR") > -1)   { appMsg.alert(data["REQUEST"]["Return"]); }
+    else if (data["REQUEST"]["Return"].indexOf("OK") > -1) { appMsg.info("<b>" + data["REQUEST"]["Command"] + "</b>: " + data["REQUEST"]["Return"], "ok"); }
+    appFW.requestAPI("GET",["list"],"",apiDeviceMovePosition);
+    }
 
 function apiDeviceMovePosition(data) {
 	remoteReload_load();
