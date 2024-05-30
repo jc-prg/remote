c9915c3d65b292a7915e6e0fce6ad7c424dffbe7
Christoph <email@ckloth.de>
2024-05-30 16:06:47 +0200

    app: moving remote position

--- app/remote-v3/rm_functions-api.js
+++ app/remote-v3/rm_functions-api.js
@@ -281,7 +281,6 @@ function apiDeviceMovePosition_get(data) {
     if (data["REQUEST"]["Return"].indexOf("ERROR") > -1)   { appMsg.alert(data["REQUEST"]["Return"]); }
     else if (data["REQUEST"]["Return"].indexOf("OK") > -1) { appMsg.info("<b>" + data["REQUEST"]["Command"] + "</b>: " + data["REQUEST"]["Return"], "ok"); }
     setTimeout(function() { appFW.requestAPI("GET",["list"],"",apiDeviceMovePosition); }, 1000 );
-    //setTimeout(function() { remoteReload_load(); }, 1000 );
     }
 
 function apiDeviceMovePosition(data) {
