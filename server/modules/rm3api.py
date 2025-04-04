ce1b2925113a00468981a7ddc53128145af559f1
Christoph <email@ckloth.de>
2025-04-04 08:12:37 +0200

    srv/app: include possibility to define a time different to ensure the time works with the right time zone and summer or winter time -> to be set in .env

--- server/modules/rm3api.py
+++ server/modules/rm3api.py
@@ -153,8 +153,8 @@ class RemoteAPI(RemoteDefaultClass):
             setting = []
 
         data["REQUEST"]["load-time"] = (time.time() - data["REQUEST"]["start-time"])
-        data["REQUEST"]["server-time"] = datetime.now().strftime("%Y-%m-%d | %H:%M | %w")
-        data["REQUEST"]["server-time-local"] = self.config.local_time().strftime("%Y-%m-%d | %H:%M | %w")
+        data["REQUEST"]["server-time"] = datetime.now().strftime("%Y-%m-%d | %H:%M | %A (%w)")
+        data["REQUEST"]["server-time-local"] = self.config.local_time().strftime("%Y-%m-%d | %H:%M | %A (%w)")
 
         if "CONFIG" in data:
             data["CONFIG"]["reload_status"] = self.queue.reload
