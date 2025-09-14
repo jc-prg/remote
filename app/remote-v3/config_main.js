a04bcc44fb80a29337b0e1e36e0fd17d30841b3d
Christoph <email@ckloth.de>
2025-09-14 17:23:19 +0200

    update version info API_version = "v2.7.0", APP_version = "v3.0.0"

--- app/remote-v3/config_main.js
+++ app/remote-v3/config_main.js
@@ -10,7 +10,7 @@ var app_setting_style_header    = "setting_bg header";
 
 var app_last_load               = 0;
 var app_title                   = "jc://remote/";
-var app_version                 = "v2.9.9";
+var app_version                 = "v3.0.0";
 var app_api_dir                 = "api/";
 var app_api_status              = "list";
 
--- server/modules/rm3presets.py
+++ server/modules/rm3presets.py
@@ -9,8 +9,8 @@ from logging.handlers import RotatingFileHandler
 # ---------------------------------
 
 API_name = "jc://remote/"
-API_version = "v2.6.1"
-APP_version = "v2.9.9"
+API_version = "v2.7.0"
+APP_version = "v3.0.0"
 APP_support = [APP_version,
                "v2.9.8", "v2.9.7"
                ]  # other supported versions
