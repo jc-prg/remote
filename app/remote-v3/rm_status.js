4ad48bab70c5033b053834b920f7a92f049d264d
Christoph <email@ckloth.de>
2025-11-02 20:18:44 +0100

    app: error msg on top of scene

--- app/remote-v3/rm_status.js
+++ app/remote-v3/rm_status.js
@@ -948,9 +948,10 @@ function statusCheck_displayValues(data={}) {
 				if (key_status && value_key == "power") {
 					//if (connected != "connected")                           { key_status = use_color("<b>"+lang("CONNECTION_ERROR")+":</b><br/>","error")+connected; }
 /// ------------> status auswerten
-					if (online_status && online_status == "offline")   { key_status = use_color("<b>"+lang("OFFLINE")+"</b><br/>","hint"); }
-			        else if (key_status.toUpperCase().indexOf("ON") >= 0)   { key_status = use_color("<b>Power On<b/>","on"); }
+					//if (online_status && online_status == "offline")   { key_status = use_color("<b>"+lang("OFFLINE")+"</b><br/>","hint"); }
+			        if (key_status.toUpperCase().indexOf("ON") >= 0)   { key_status = use_color("<b>Power On<b/>","on"); }
 					else if (key_status.toUpperCase().indexOf("OFF") >= 0)  { key_status = use_color("<b>Power Off<b/>","hint"); }
+					else if (key_status.toUpperCase().indexOf("N/A") >= 0)  { key_status = use_color("<b>Power Status N/A<b/>","hint"); }
                     else                                                    { key_status = use_color("<b>"+lang("ERROR_UNKNOWN")+":</b> ","error")+key_status; }
 					}
 
