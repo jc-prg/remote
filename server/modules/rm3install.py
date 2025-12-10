7f94e19c431a6e32bb2c104fbfa0d763e66d2a94
Christoph <email@ckloth.de>
2025-12-10 18:06:16 +0100

    enhance start script

--- server/modules/rm3install.py
+++ server/modules/rm3install.py
@@ -1,34 +1,32 @@
 import os
 import shutil
 import server.modules.rm3presets as rm3presets
-from server.modules.rm3classes import RemoteDefaultClass
 
 
-class RemoteInstall(RemoteDefaultClass):
+class RemoteInstall():
     """
     Class to check whether relevant config and data exist, install from sample files if not.
     Important: the .env file is not part of this installation, as this file is minimum requirement to start docker-compose.
     """
 
     def __init__(self):
-        RemoteDefaultClass.__init__(self, "install", "Installation Check")
-
         self.directory_main = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
         self.directory_data = rm3presets.data_dir
         self.directory_sample = os.path.abspath(os.path.join(self.directory_main, "data/_sample/"))
 
-        self.config_files = [
-            {"type": "directory", "path": self.directory_data, "action": "create"},
-            {"type": "directory", "path": os.path.join(self.directory_data, rm3presets.remotes), "action": "copy", "source": os.path.join(self.directory_sample, rm3presets.remotes)},
-            {"type": "directory", "path": os.path.join(self.directory_data, rm3presets.devices), "action": "copy", "source": os.path.join(self.directory_sample, rm3presets.devices)},
-            {"type": "directory", "path": os.path.join(self.directory_data, rm3presets.templates), "action": "copy", "source": os.path.join(self.directory_sample, rm3presets.templates)},
-            {"type": "json", "path": os.path.join(self.directory_data, rm3presets.active_devices), "action": "info"},
-            {"type": "json", "path": os.path.join(self.directory_data, rm3presets.active_scenes), "action": "info"},
-            {"type": "json", "path": os.path.join(self.directory_data, rm3presets.active_macros), "action": "info"},
-            {"type": "json", "path": os.path.join(self.directory_data, rm3presets.active_apis), "action": "copy", "source": os.path.join(self.directory_sample, rm3presets.active_apis)},
-            {"type": "json", "path": os.path.join(self.directory_data, rm3presets.active_timer), "action": "copy", "source": os.path.join(self.directory_sample, rm3presets.active_timer)},
-            {"type": "json", "path": os.path.join(self.directory_data, rm3presets.active_device_types),"action": "copy", "source": os.path.join(self.directory_sample, rm3presets.active_device_types)},
-        ]
+    self.config_files = [
+        {"type": "log", "path": os.path.join(self.directory_data, rm3presets.log_filename), "action": "create"},
+        {"type": "directory", "path": self.directory_data, "action": "create"},
+        {"type": "directory", "path": os.path.join(self.directory_data, rm3presets.remotes), "action": "copy", "source": os.path.join(self.directory_sample, rm3presets.remotes)},
+        {"type": "directory", "path": os.path.join(self.directory_data, rm3presets.devices), "action": "copy", "source": os.path.join(self.directory_sample, rm3presets.devices)},
+        {"type": "directory", "path": os.path.join(self.directory_data, rm3presets.templates), "action": "copy", "source": os.path.join(self.directory_sample, rm3presets.templates)},
+        {"type": "json", "path": os.path.join(self.directory_data, rm3presets.active_devices), "action": "info"},
+        {"type": "json", "path": os.path.join(self.directory_data, rm3presets.active_scenes), "action": "info"},
+        {"type": "json", "path": os.path.join(self.directory_data, rm3presets.active_macros), "action": "info"},
+        {"type": "json", "path": os.path.join(self.directory_data, rm3presets.active_apis), "action": "copy", "source": os.path.join(self.directory_sample, rm3presets.active_apis)},
+        {"type": "json", "path": os.path.join(self.directory_data, rm3presets.active_timer), "action": "copy", "source": os.path.join(self.directory_sample, rm3presets.active_timer)},
+        {"type": "json", "path": os.path.join(self.directory_data, rm3presets.active_device_types),"action": "copy", "source": os.path.join(self.directory_sample, rm3presets.active_device_types)},
+    ]
 
     def check_configuration(self):
         """
@@ -38,7 +36,6 @@ class RemoteInstall(RemoteDefaultClass):
         Returns:
             bool: True if everything exists or could be created
         """
-        self.logging.info("Check if configuration is complete ...")
         print("\nCheck if configuration is complete ...")
 
         for entry in self.config_files:
@@ -48,11 +45,12 @@ class RemoteInstall(RemoteDefaultClass):
                     entry["source"] += ".json"
 
             if os.path.exists(entry["path"]):
-                self.logging.info(f"- {entry["type"]} exists: {entry["path"]}")
                 print(f"- {entry["type"]} exists: {entry["path"]}")
             else:
-                self.logging.warning(f"- {entry["type"]} doesn't exist: {entry["path"]}")
                 print(f"- {entry["type"]} doesn't exist: {entry["path"]}")
+
+                if entry["log"] == "log":
+                    self.create_log_file(entry)
 
                 if entry["type"] == "directory":
                     self.create_copy_directory(entry)
@@ -70,16 +68,30 @@ class RemoteInstall(RemoteDefaultClass):
         if entry["action"] == "create" and "source" not in entry:
             os.mkdir(entry["path"])
             if os.path.exists(entry["path"]):
-                self.logging.info(f"  -> OK: created {entry["type"]}.")
+                print(f"  -> OK: created {entry["type"]}.")
             else:
-                self.logging.error(f"  -> ERROR: could not create {entry["type"]}.")
+                print(f"  -> ERROR: could not create {entry["type"]}.")
 
         elif entry["action"] == "create":
             try:
                 shutil.copytree(entry["source"], entry["path"])
-                self.logging.info(f"  -> OK: copied content from source {entry["source"]}.")
+                print(f"  -> OK: copied content from source {entry["source"]}.")
             except Exception as e:
-                self.logging.error(f"  -> ERROR: could not copy content from source {e}.")
+                print(f"  -> ERROR: could not copy content from source {e}.")
+
+    def create_log_file(self, entry):
+        """
+        create empty log files
+        """
+        if entry["action"] == "create":
+            try:
+                open(entry["path"], "w").close()
+                if os.path.exists(entry["path"]):
+                    print(f"  -> OK: created empty log file")
+                else:
+                    print(f"  -> ERROR: could not create log file {entry["path"]}")
+            except Exception as e:
+                print(f"  -> ERROR: could not create log file {e}")
 
     def create_copy_file(self, entry):
         if entry["action"] == "copy":
