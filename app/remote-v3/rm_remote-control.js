95e7ff1d85cecfebd46ebaed2b861bea2c1d79b2
Christoph <email@ckloth.de>
2026-01-05 20:24:09 +0100

    app: bugfix button svg text for iphones

--- app/remote-v3/rm_remote-control.js
+++ app/remote-v3/rm_remote-control.js
@@ -60,7 +60,7 @@ class RemoteSvgTextImage extends RemoteDefaultClass {
         console.error(box, isIPhone);
 
         if (line_count > 1 && isIPhone) {
-            line_count = line_count * 0.75;
+            line_count = 1; //line_count * 0.75;
             box.height = box.height * line_count;
             box.width = box.width * line_count;
         }
